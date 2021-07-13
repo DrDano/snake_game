f7b94a7fdbda2f2590103f69d2c87bfe86fa001d
DrDano <danielharned@gmail.com>
2021-07-12 17:00:11 -0700

    began code for combining turtles to make snake (Day 20 of 100DoC) 071221

--- main.py
+++ main.py
@@ -5,5 +5,11 @@ screen.setup(width=600, height=600)
 screen.bgcolor('black')
 screen.title('Snake Game')
 
+ted = Turtle()
+turtle_num = 0
+for turtle_name in range(3):
+
+
+
 
 screen.exitonclick()
