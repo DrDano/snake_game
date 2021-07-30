97aae2b8b557440e01a0319b403f11a7ca02a3df
DrDano <danielharned@gmail.com>
2021-07-30 06:56:48 -0700

    snake game complete but with commented code in main and crippling problem with extend function involving the position function from turtle library (Day 21 100DoC) 072821

--- main.py
+++ main.py
@@ -22,19 +22,30 @@ screen.onkey(key='Right', fun=snake.right)
 
 game_is_on = True
 while game_is_on:
+    snake.move()
     screen.update()
     time.sleep(0.1)
-    snake.move()
 
-    #detect collision with food.
+    # detect collision with food.
     if snake.segments[0].distance(food) < 15:
         food.refresh()
         snake.extend()
         scoreboard.increase_score()
 
-    #Detect collision with wall.
-    if snake.segments[0].xcor() > 280 or snake.segments[0].xcor() < -280 or snake.segments[0].ycor() > 280 or snake.segments[0].ycor() < -280:
+    # Detect collision with wall.
+    if snake.segments[0].xcor() > 280 or snake.segments[0].xcor() < -280 \
+            or snake.segments[0].ycor() > 280 or snake.segments[0].ycor() < -280:
         game_is_on = False
         scoreboard.game_over()
+
+    # Detect collision with tail.
+    # If the head collides with any segment in the tail:
+        # trigger game_over
+    # for segment in snake.segments:
+    #     if segment == snake.segments[0]:
+    #         pass
+    #     elif snake.segments[0].distance(segment) > 10:
+    #         game_is_on = False
+    #         scoreboard.game_over()
 
 screen.exitonclick()
--- snake.py
+++ snake.py
@@ -1,10 +1,10 @@
 from turtle import Turtle, Screen
-PACES = 20
+PACES = 10
 UP = 90
 DOWN = 270
 LEFT = 180
 RIGHT = 0
-POSITIONS = [(0, 0), (-10, 0), (-20, 0)]
+POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 
 class Snake:
 
@@ -13,8 +13,8 @@ class Snake:
         self.create_snake()
 
     def create_snake(self):
-        for position in POSITIONS:
-            self.add_segment(position)
+        for p in POSITIONS:
+            self.add_segment(p)
 
     def add_segment(self, position):
         new_segment = Turtle()
@@ -23,9 +23,12 @@ class Snake:
         new_segment.penup()
         new_segment.goto(position)
         self.segments.append(new_segment)
+        print(position)
 
     def extend(self):
         self.add_segment(self.segments[-1].position())
+        # The position function is where this code suffers from. It tacks on the turtle's position...
+        # but it tacks it on almost too late? the snake segments are spaced out.
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
